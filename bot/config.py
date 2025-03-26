cd7e091bc5dc6b1c9c41892b2e6824bc1e4b92d5
Deyski999 <ghaith.alrahi@gmail.com>
2025-03-26 10:57:06 +0200

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = config_env["MONGODB_URI"]
+mongodb_uri = "mongodb+srv://ghaithalrahi:Searchersof99!@deyski999.j1l0sp5.mongodb.net/?retryWrites=true&w=majority&appName=Deyski999"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
